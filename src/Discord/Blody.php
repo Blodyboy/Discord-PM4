b8460cf91c71c8909553d8c9e6cf36eba6b76e99
Blodyboy <132788148+Blodyboy@users.noreply.github.com>
2023-05-07 11:15:49 +0300

    Add files via upload

---
+++ src/Discord/Blody.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace discord;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
+use pocketmine\utils\Config;
+
+class Blody extends PluginBase {
+    public function onLoad(): void
+    {
+        $this->saveResource("config.yml");
+    }
+
+    public function onEnable(): void {
+      $this->getLogger()->info("Discord Plugin aktif - Blody");
+    }
+    public function onDisable(): void{ 
+      $this->getLogger()->info("Discord Plugin De-aktif - Blody");
+    }
+    public function onCommand(CommandSender $player, Command $kmt, string $label, array $args): bool {
+       if($kmt->getName() == "discord") {
+           if ($player instanceof Player) {
+            $config = new Config($this->getDataFolder(). "config.yml", 2);   
+            $player->sendMessage($config->get("mesaj"));
+        }else $player->sendMessage("Bu Komut Sadece Oyunda Kullanılabilir.");
+       }
+        return true;
+    }
+} 
